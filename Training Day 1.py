54817be5e4c373a0b86de8e47b41fdcc6eeb91e0
michaeldejesus <47112702+michaeldejesus@users.noreply.github.com>
2019-01-28 13:31:57 -0600

    Add files via upload

---
+++ Training Day 1.py
@@ -0,0 +1,8 @@
+# This is a comment. Comments begin with a #
+# Default usage of the print() is as follows:
+# print (*objects, sep=' ', end='\n', file=sys.sys.stdout, fluch=False)
+#
+print("Hello, World!")
+print('good bye world...')
+print('what is all this for?')
+print('I am tired!')
